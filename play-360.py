e28326534ca5c2b7a9d912f6342e1f49daa6f0dc
E Rogers <ed.rogers@bristolbraille.org>
2022-07-22 15:32:50 +0100

    Fixed oscilating map screen bug...
    ... Where if a road was at the edge of the map traversing it
    would mean switching maps screens every step. FIxed by checking
    direction of moment.
    
    Also improved variable layout.

--- play-360.py
+++ play-360.py
@@ -44,11 +44,11 @@ class Game(object):
 		self.momentum[1] = 0
 		self.transport = 'parked'
 
-	def move_player(self, (dx, dy), left_col, top_row, highway_csv, look_for_story):
+	def move_player(self, (dx, dy), map_change):
 
 		blocked = False
 
-		pos=[left_col + self.x,top_row + self.y]
+		pos=[self.map_pos[0] + self.x,self.map_pos[1] + self.y]
 		neighbours = [
 			MAP[pos[1]-1][pos[0]],
 			MAP[pos[1]-1][pos[0]+1],
@@ -111,21 +111,21 @@ class Game(object):
 			raise BlockedMovement()
 		else: self.transport = 'driving'
 
-		for highway_row in highway_csv:
-			if (int(highway_row[1])-1 == int(left_col+self.x) and
-				int(highway_row[0])-1 == int(top_row+self.y)):
+		for highway_row in self.ways:
+			if (int(highway_row[1])-1 == int(self.map_pos[0]+self.x) and
+				int(highway_row[0])-1 == int(self.map_pos[1]+self.y)):
 				self.location = highway_row[2]
-		if look_for_story == True:
+		if map_change == False:
 			for story_row in self.story_csv:
 				# First cell is whether it is unlocked by default or has
 				# to be triggered by another event (1/0)
-				if (int(story_row[1])-1 == int(left_col+self.x) and
-					int(story_row[2])-1 == int(top_row+self.y)):
+				if (int(story_row[1])-1 == int(self.map_pos[0]+self.x) and
+					int(story_row[2])-1 == int(self.map_pos[1]+self.y)):
 					self.story(story_row[3])
 
-	def draw_map(self, (left_col, top_row)):
+	def draw_map(self):
 		for row in range(8):
-			self.screen.addstr(row, 0, MAP[top_row + row][left_col:left_col+40])
+			self.screen.addstr(row, 0, MAP[self.map_pos[1] + row][self.map_pos[0]:self.map_pos[0]+40])
 
 	def story(self, story_page):
 		# If the story location is two cells from top it triggers three times...
@@ -134,33 +134,38 @@ class Game(object):
 		# Mark story page as read
 		# Look for related story pages on same csv row and unmark them as usable
 		# That way a basic story telling multiple choice game engine.
-#		clear()
+		clear()
 		for row in range(8):
-			self.screen.addstr(row, 0, storylines[row])
-			stdscr.timeout(2000)
+			self.screen.addstr(row, 0, storylines[row][0:39])
+			stdscr.timeout(self.speed)
 			self.screen.getch()
 		self.break_movement()
 		stdscr.timeout(720000)
 		self.screen.getch()
 
 	def main(self):
-		key = None
 		with open(map_dir+'map-start.csv', mode='r') as map_start_file:
 			map_start_csv = list(csv.reader(map_start_file))
+		self.map_pos = [
+			int(map_start_csv[1][0]),
+			int(map_start_csv[1][1])
+		]
+		self.location = map_start_csv[1][2]
+		self.x, self.y = int(map_start_csv[0][0]), int(map_start_csv[0][1])
 		with open(map_dir+'highway-locations.csv', mode='r') as highway_locations_file:
-			highway_csv = list(csv.reader(highway_locations_file))
+			self.ways = list(csv.reader(highway_locations_file))
+		self.transport = 'parked'
+		self.momentum = [0,0]
+		self.speed = 3500
 		with open(map_dir+'story.csv', mode='r') as story_file:
 			self.story_csv = list(csv.reader(story_file))
-		self.x, self.y = int(map_start_csv[0][0]), int(map_start_csv[0][1])
-		self.transport = 'parked'
-		self.location = map_start_csv[1][2]
-		map_left_col = int(map_start_csv[1][0])
-		map_top_row = int(map_start_csv[1][1])
-		self.direction = [0,0]
-		self.momentum = [0,0]
 		self.story(self.story_csv[0][3])
+		direction = [0,0]
+		map_change = False
+		key = None
 		while key != KEY_QUIT:
-			stdscr.timeout(2000)
+			if map_change == True: stdscr.timeout(10000+self.speed)
+			else: stdscr.timeout(self.speed)
 			key = self.screen.getch()
 			try: self.direction = DIRECTIONS[key]
 			except KeyError: self.direction = [0,0]
@@ -170,27 +175,32 @@ class Game(object):
 			if key == KEY_BREAK: self.break_movement()
 			self.screen.addstr(self.y, self.x, ' ')
 			if self.momentum[0] != 0 or self.momentum[1] != 0:
+				map_change = False
 				try:
-					self.move_player(self.momentum, map_left_col, map_top_row, highway_csv, True)
+					self.move_player(self.momentum, map_change)
 				except BlockedMovement: self.break_movement()
 				pass
-				if self.x < 2:
-					map_left_col = map_left_col-38
-					if map_left_col < 0: map_left_col = 0
+				if self.x < 2 and self.momentum[0] < 0:
+					self.map_pos[0] = self.map_pos[0]-38
+					if self.map_pos[0] < 0: self.map_pos[0] = 0
 					else:
-						self.move_player((+38, 0), map_left_col, map_top_row, highway_csv, False)
-				if self.x > 38:
-					map_left_col = map_left_col+38
-					self.move_player((-38,0), map_left_col, map_top_row, highway_csv, False)
-				if self.y < 2:
-					map_top_row = map_top_row-6
-					if map_top_row < 0: map_top_row = 0
+						map_change = True
+						self.move_player((+38, 0), map_change)
+				elif self.x > 38 and self.momentum[0] > 0:
+					self.map_pos[0] = self.map_pos[0]+38
+					map_change = True
+					self.move_player((-38,0), map_change)
+				if self.y < 2 and self.momentum[1] < 0:
+					self.map_pos[1] = self.map_pos[1]-6
+					if self.map_pos[1] < 0: self.map_pos[1] = 0
 					else:
-						self.move_player((0, +6), map_left_col, map_top_row, highway_csv, False)
-				if self.y > 6:
-					map_top_row = map_top_row+6
-					self.move_player((0, -6), map_left_col, map_top_row, highway_csv, False)
-			self.draw_map([map_left_col, map_top_row])
+						map_change = True
+						self.move_player((0, +6), map_change)
+				elif self.y > 6 and self.momentum[1] > 0:
+					self.map_pos[1] = self.map_pos[1]+6
+					map_change = True
+					self.move_player((0, -6), map_change)
+			self.draw_map()
 			self.add_message('You are '+self.transport+' on '+self.location)
 			self.screen.addstr(self.y, self.x, '=')
 			# Hack to move cursor out the way
